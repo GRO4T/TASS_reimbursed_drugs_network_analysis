3d976e2833b9159a0317a4c9df12ba1d7c5d78f5
SwirtaB <49533902+SwirtaB@users.noreply.github.com>
2024-01-03 22:08:42 +0100

    data preparation, bipartite graph creation

---
+++ network_util.py
@@ -0,0 +1,23 @@
+import networkx as nx
+
+
+def create_bipartite_graph(set_0, set_1, weights=None) -> nx.Graph:
+    graph = nx.Graph()
+    graph.add_nodes_from(set_0, bipartite=0)
+    graph.add_nodes_from(set_1, bipartite=1)
+    if weights is not None:
+        edges = list(zip(set_0, set_1, weights))
+        graph.add_weighted_edges_from(edges)
+    else:
+        edges = list(zip(set_0, set_1))
+        graph.add_edges_from(edges)
+
+    return graph
+
+
+def create_bipartite_layout(set_0, set_1) -> dict:
+    layout = {}
+    layout.update((n, (1, i)) for i, n in enumerate(set_0))
+    layout.update((n, (2, i)) for i, n in enumerate(set_1))
+
+    return layout
